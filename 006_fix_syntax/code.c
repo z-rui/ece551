5e682cf68deb1241f623a57baf14c09c15da20da
rz78 <rz78@duke.edu>
2017-09-02 16:18:44 -0400

    Submit 006

--- 006_fix_syntax/code.c
+++ 006_fix_syntax/code.c
@@ -1,8 +1,8 @@
                                  //line 1
 int f(int x) {                   //line 2
-  int answer = 0                 //line 3
-  for (int i = 0; i < X; i++) {  //line 4
+  int answer = 0;                //line 3
+  for (int i = 0; i < x; i++) {  //line 4
     answer += i * i;             //line 5
   }                              //line 6
   return answer;                 //line 7
-                                 //line 8
+}                                //line 8
