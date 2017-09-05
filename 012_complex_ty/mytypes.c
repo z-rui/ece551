925ce3ba2772497f7b413d3b7fa663febab2a0ae
rz78 <rz78@duke.edu>
2017-09-05 19:05:25 -0400

    Submit 012

---
+++ 012_complex_ty/mytypes.c
@@ -0,0 +1,16 @@
+struct _complex_num_t {
+	double real_part;
+	double imaginary_part;
+};
+
+typedef struct _complex_num_t complex_num_t;
+
+enum _animal_t {
+	CAT,
+	FISH,
+	SNAKE,
+	DOG,
+	HORSE,
+};
+
+typedef enum _animal_t animal_t;
