cb6eff13988b27b3f8b1c8041a49302749a9da0c
rz78 <rz78@duke.edu>
2017-09-06 14:16:51 -0400

    Submit 014

--- 014_code1/code1.c
+++ 014_code1/code1.c
@@ -1,7 +1,12 @@
 int max (int num1, int num2) {
   //check if num1 is greater than num2
+  if (num1 > num2) {
     //if so, your answer is num1
+    return num1;
+  } else {
     //otherwise, your answer is num2
+    return num2;
+  }
 }
 
 int main(void) {
@@ -9,8 +14,9 @@ int main(void) {
   printf("max(33, 0) is %d\n", max(33, 0));
   printf("max(0x123456, 123456) is %d\n", max(0x123456, 123456));
   //print the max of 0x451215AF and 0x913591AF and prints it out as a decimal number
+  printf("max(0x451215AF, 0x913591AF) is %d\n", max(0x451215AF, 0x913591AF));
   
   return 0;
 }
 
-
+// vim: ts=2:sw=2:et
