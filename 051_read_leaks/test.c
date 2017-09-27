a6e9817102dab287c59bc15c234b75fcb2eba7a6
rz78 <rz78@duke.edu>
2017-09-27 13:43:00 -0400

    Released 051_read_leaks

---
+++ 051_read_leaks/test.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int f(int n) {
+  int * p = malloc(2 * sizeof(*p));
+  p[0] = n;
+  p[1] = n+2;
+  int ans = p[0] * p[1];
+  return ans;
+}
+
+int main(void) {
+  int * p = malloc(4 * sizeof(*p));
+  int * q = p;
+  int ** r = &q;
+  p[0] = f(1);
+  *r = NULL;
+  q = malloc(2 * sizeof(*q));
+  p = q;
+  q = NULL;
+  return EXIT_SUCCESS;
+}
