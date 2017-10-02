e73249a6333b5de11fceb9885f355f17d8df19de
rz78 <rz78@duke.edu>
2017-10-02 14:10:42 -0400

    Submit 057

--- 057_outname/outname.c
+++ 057_outname/outname.c
@@ -4,5 +4,14 @@
 #include "outname.h"
 
 char * computeOutputFileName(const char * inputName) {
-  //WRITE ME
+  size_t len;
+  char *outName;
+
+  len = strlen(inputName);
+  outName = malloc(len + 8); // . c o u n t s \0
+  memcpy(outName, inputName, len);
+  memcpy(outName + len, ".counts", 8);
+  return outName;
 }
+
+// vim: ts=2:sw=2:et
