cbb64a5d967924e51824804c5e71a125c7be8aaf
rz78 <rz78@duke.edu>
2017-10-17 19:04:03 -0400

    Submit 082

---
+++ 082_expr_eval2/expr.h
@@ -0,0 +1,55 @@
+#include <string>
+#include <sstream>
+
+class Expression {
+public:
+	virtual std::string toString() const = 0;
+	virtual ~Expression() {}
+};
+
+class NumExpression : public Expression {
+	long numVal;
+public:
+	NumExpression(long v) : numVal(v) {}
+	virtual std::string toString() const {
+		std::stringstream ss;
+		ss << numVal;
+		return ss.str();
+	}
+};
+
+class BinOpExpression : public Expression {
+	const char *theOp; // always points to a literal
+	Expression *lhs, *rhs; // owned by the object
+public:
+	BinOpExpression(const char *op, Expression *lhs, Expression *rhs) : theOp(op), lhs(lhs), rhs(rhs) {}
+	virtual std::string toString() const {
+		std::stringstream ss;
+		ss << '(' << lhs->toString() << ' ' << theOp << ' ' << rhs->toString() << ')';
+		return ss.str();
+	}
+	virtual ~BinOpExpression() {
+		delete lhs;
+		delete rhs;
+	}
+};
+
+class PlusExpression : public BinOpExpression {
+public:
+	PlusExpression(Expression *lhs, Expression *rhs) : BinOpExpression("+", lhs, rhs) {}
+};
+
+class MinusExpression : public BinOpExpression {
+public:
+	MinusExpression(Expression *lhs, Expression *rhs) : BinOpExpression("-", lhs, rhs) {}
+};
+
+class TimesExpression : public BinOpExpression {
+public:
+	TimesExpression(Expression *lhs, Expression *rhs) : BinOpExpression("*", lhs, rhs) {}
+};
+
+class DivExpression : public BinOpExpression {
+public:
+	DivExpression(Expression *lhs, Expression *rhs) : BinOpExpression("/", lhs, rhs) {}
+};
