65d1a2a174f8a94af73068bf842995926cbe64bf
rz78 <rz78@duke.edu>
2017-11-04 00:39:58 -0400

    Submit 101

--- 101_huff_map/node.cpp
+++ 101_huff_map/node.cpp
@@ -1,5 +1,11 @@
 #include "node.h"
 void Node::buildMap(BitString b, std::map<unsigned,BitString> & theMap) {
-  //WRITE ME!
+  if (sym != NO_SYM) {
+    theMap[sym] = b;
+  } else {
+    left->buildMap(b.plusZero(), theMap);
+    right->buildMap(b.plusOne(), theMap);
+  }
 }
 
+// vim: sw=2:ts=2:et
