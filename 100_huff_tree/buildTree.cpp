3d56cb9d423883ae583c03afacfc97ae8b87a0e2
rz78 <rz78@duke.edu>
2017-11-04 00:31:58 -0400

    Submit 101

--- 100_huff_tree/buildTree.cpp
+++ 100_huff_tree/buildTree.cpp
@@ -1,5 +1,20 @@
 #include "node.h"
 
 Node *  buildTree(uint64_t * counts) {
-  //WRITE ME!
-}  
+  priority_queue_t pq;
+  for (int i = 0; i <= 256; i++) {
+    if (counts[i] > 0) {
+      pq.push(new Node(i, counts[i]));
+    }
+  }
+  while (pq.size() > 1) {
+    Node *l = pq.top();
+    pq.pop();
+    Node *r = pq.top();
+    pq.pop();
+    pq.push(new Node(l, r));
+  }
+  return pq.top();
+}
+
+// vim: ts=2:sw=2:et
