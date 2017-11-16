d65734f8e7c6e68500f17d30da6056bc534f1f0e
rz78 <rz78@duke.edu>
2017-11-15 19:35:53 -0500

    Submit miniproject - parser supports variables
    Expansion, parsing set and export commands

--- mp_miniproject/parse.cpp
+++ mp_miniproject/parse.cpp
@@ -1,6 +1,11 @@
 #include <ctype.h> /* for isspace, etc. */
+#include <string.h> /* for strcmp */
 
 #include "parse.h"
+
+Parser::Parser(const VarTab &vtab) : vtab(vtab)
+{
+}
 
 /* Parser::parse parses one line.
  *
@@ -8,6 +13,11 @@
 bool Parser::parse(const char *line, Parser::Pipes& pipes)
 {
 	pos = line;
+	pos_before_expansion = NULL;
+
+	scannedTerms.clear();
+	specials.redir = specials.space =
+		specials.expand = specials.escape = 1;
 
 	return parsePipes(pipes) && endOfLine();
 }
@@ -25,7 +35,7 @@ bool Parser::parsePipes(Parser::Pipes& pipes)
 	}
 	for (;;) {
 		pipes.push_back(Command());
-		if (!parseCommand(pipes.back()))
+		if (parseCommand(pipes.back()) == Command::INVALID)
 			return false; // failed
 
 		skipSpaces();
@@ -42,10 +52,11 @@ bool Parser::parsePipes(Parser::Pipes& pipes)
  * command ::= command-term { command-term }
  * command-term ::= TERM | '<' TERM | '>' TERM | "2>" TERM
  */
-bool Parser::parseCommand(Parser::Command& cmd)
+Parser::Command::Type Parser::parseCommand(Parser::Command& cmd)
 {
+	bool first = true;
 	for (;;) {
-		char ch;
+		unsigned char ch;
 		int redir = -1; /* -1: no redir; 0,1,2: redir fd 0,1 or 2 */
 
 		skipSpaces();
@@ -62,21 +73,70 @@ bool Parser::parseCommand(Parser::Command& cmd)
 		}
 		const char *term = scanTerm();
 		if (term == NULL) { // failed
-			return false;
+			return Command::INVALID;
 		}
 		if (redir >= 0) {
 			cmd.redir[redir] = term;
 		} else {
 			cmd.argv.push_back(term);
 		}
+		if (first) {
+			first = false;
+			Command::Type builtin = parseBuiltin(term, cmd);
+			if (builtin != Command::INVALID) {
+				return builtin;
+			}
+		}
 
 		skipSpaces();
-		if (endOfLine() || peek() == '|') {
+		if (endOfLine() || peek() == '|') { // end of a command
 			// argv ends with a NULL
 			cmd.argv.push_back(NULL);
-			return true;
+			return Command::ORDINARY;
 		}
 	}
+}
+
+Parser::Command::Type Parser::parseBuiltin(const char *cmdname,
+		Parser::Command& cmd)
+{
+	if (strcmp(cmdname, "set") == 0) {
+		return parseSetCommand(cmd);
+	} else if (strcmp(cmdname, "export") == 0) {
+		return parseExportCommand(cmd);
+	}
+	return Command::INVALID;
+}
+
+Parser::Command::Type Parser::parseSetCommand(Parser::Command& cmd)
+{
+	const char *name, *value;
+
+	name = scanName();
+	if (name == NULL) {
+		return Command::INVALID;
+	}
+	// redir and space are inactive inside variable definition
+	specials.redir = specials.space = 0;
+	value = scanTerm();
+	if (value == NULL) {
+		return Command::INVALID;
+	}
+	cmd.argv.push_back(name);
+	cmd.argv.push_back(value);
+	return Command::SET;
+}
+
+Parser::Command::Type Parser::parseExportCommand(Parser::Command& cmd)
+{
+	const char *name;
+
+	name = scanName();
+	if (name == NULL) {
+		return Command::INVALID;
+	}
+	cmd.argv.push_back(name);
+	return Command::EXPORT;
 }
 
 /* Parser::scanTerm scans a term in a command.
@@ -95,23 +155,42 @@ const char *Parser::scanTerm()
 	const char *save = pos;
 	bool terminate = false;
 	do {
-		char ch = peek();
+		unsigned char ch = peek();
+		const char *name;
 		switch (ch) {
-			case '\0':
-			case ' ': case '\t': case '\n': case '\f': case '\v':
-			case '<': case '>': case '|':
-				terminate = true;
-				break;
-			case '\\':
-				next(); // skip backslash
+		case '\0':
+			terminate = true;
+			break;
+		case '<': case '>': case '|':
+			terminate = specials.redir;
+			break;
+		case ' ': case '\t': case '\v': case '\f': case '\n':
+			terminate = specials.space;
+			break;
+		case '$':
+			if (!specials.expand) {
+				break; // expansion disabled
+			}
+			next(); // skip '$'
+			name = scanName();
+			if (name != NULL) {
+				enterExpansion(name);
+			} else { // a stray '$'
+				term.push_back(ch);
+			}
+			break;
+		case '\\':
+			if (specials.escape) {
+				next(); // skip '\'
 				if ((ch = peek()) == '\0') {
 					break;
 				}
-				/* FALLTHROUGH */
-			default:
-				term.push_back(ch);
-				next();
-				break;
+			}
+			/* FALLTHROUGH */
+		default:
+			term.push_back(ch);
+			next();
+			break;
 		}
 	} while (!terminate);
 
@@ -121,9 +200,54 @@ const char *Parser::scanTerm()
 	return term.c_str();
 }
 
+const char *Parser::scanName()
+{
+	unsigned char ch = peek();
+	if (!isalpha(ch)) {
+		return NULL;
+	}
+	scannedTerms.push_back(std::string());
+	std::string& name = scannedTerms.back();
+	do {
+		name.push_back(ch);
+		next();
+		ch = peek();
+	} while (isalnum(ch));
+	return name.c_str();
+}
+
+void Parser::enterExpansion(const char *name)
+{
+	const char *value = vtab.getVar(name);
+	if (value == NULL || value[0] == '\0') {
+		// empty expansion; exit immediately
+		return;
+	}
+	pos_before_expansion = pos;
+	pos = value;
+	specials_before_expansion = specials;
+	specials.redir = specials.expand = specials.escape = 0;
+}
+
+void Parser::exitExpansion()
+{
+	pos = pos_before_expansion;
+	pos_before_expansion = NULL;
+	specials = specials_before_expansion;
+}
+
 void Parser::skipSpaces()
 {
 	while (isspace(peek())) {
 		next();
+	}
+}
+
+void Parser::next(int n)
+{
+	pos += n;
+	if (*pos == '\0' && insideExpansion()) {
+		// just finished an expansion
+		exitExpansion();
 	}
 }
--- mp_miniproject/parse.h
+++ mp_miniproject/parse.h
@@ -5,26 +5,49 @@
 #include <list>
 #include <vector>
 
+#include <limits.h>
+
+#include "var.h"
+
 class Parser {
 public:
 	struct Command {
+		enum Type {
+			INVALID = -1,
+			ORDINARY, SET, EXPORT,
+		};
+		Type type;
 		std::vector<const char *> argv;
 		const char *redir[3];
 	};
 	typedef std::list<Command> Pipes;
-	bool parse(const char *, Pipes&);
 
+	Parser(const VarTab&);
+	bool parse(const char *, Pipes&);
 private:
+	const VarTab& vtab;
 	std::list<std::string> scannedTerms;
 	const char *pos;
+	const char *pos_before_expansion;
+	struct Specials {
+		unsigned redir:1, space:1, expand:1, escape:1;
+	};
+	Specials specials, specials_before_expansion;
 
+	bool insideExpansion() const { return pos_before_expansion != NULL; }
 	bool endOfLine() const { return *pos == '\0'; }
-	char peek(int n = 0) const { return pos[n]; }
-	void next(int n = 1) { pos += n; }
+	unsigned char peek(int n = 0) const { return pos[n]; }
+	void next(int n = 1);
 	void skipSpaces();
-	bool parseCommand(Command&);
+	Command::Type parseCommand(Command&);
+	Command::Type parseBuiltin(const char *, Command&);
+	Command::Type parseSetCommand(Command&);
+	Command::Type parseExportCommand(Command&);
 	bool parsePipes(Pipes&);
 	const char *scanTerm();
+	const char *scanName();
+	void enterExpansion(const char *);
+	void exitExpansion();
 };
 
 
